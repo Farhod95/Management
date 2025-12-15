73bdb3e88a7d493529bdabe2823a6be42efde99a
Durbekuz <durbekkanalbekov74@gmail.com>
2025-12-15 21:26:19 +0500

    Create Infrastructure

---
+++ Management.Infrastucture/Data/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Management.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Management.Infrastucture.Data
+{
+    public class DbContext
+    {
+     public Student
+    }
+}
