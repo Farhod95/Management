8502cf300d265e81c8c5fdf6d429ddf28df78486
Farhod <farhodbekisomiddinov5@gmail.com>
2025-12-16 11:53:48 +0500

    added Application and added infrastructure

---
+++ Management.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Management.Application;
+
+namespace Management.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentService = new StudentService();
+            studentService.AddStudent("Karim", "Soliyev");
+            studentService.AddStudent("Isroil", "Abduraxmanov");
+
+        }
+    }
+}
---
+++ Management.Infrastructure/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Management.Domain.Models;
+
+namespace Management.Infrastructure.Data
+{
+    public class DbContext
+    {
+        public DbContext()
+        {
+            this.Students = new Student[12];
+        }
+        public Student[] Students { get; set; }
+        public int StudentCount { get; set; } = 0;
+    }
+}
